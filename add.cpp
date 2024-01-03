d417b71fe57f97ae211ee58e4a41e84056485b1a
rajptjayveer <jayveersolanki2006@gmail.com>
2024-01-03 22:01:33 +0530

    New File Added

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+main(){
+    int a, b;
+    cout<<endl<<"Enter two Numbers => ";
+    cin>>a>>b;
+
+    cout<<endl<<"Addition => "<<a+b;
+}
--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,3 @@ using namespace std;
 main(){
     cout<<"Hello! Jayveer";
 }
-
-// echo "# C-_First_Repo" >> README.md
